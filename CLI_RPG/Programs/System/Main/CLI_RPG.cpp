f01bb8f8ac78c5cc6a33dffa279b6cb0736442bc
Shaf <KDT24SGC40005@edu.kobe-tech.ac.jp>
2025-12-06 23:14:39 +0900

    最初に必要なことなので、mainブランチでします。mainの作成

--- CLI_RPG/Programs/System/Main/CLI_RPG.cpp
+++ CLI_RPG/Programs/System/Main/CLI_RPG.cpp
@@ -1,6 +1,13 @@
-﻿#include <iostream>
+﻿#ifdef _DEBUG
+#define ENTRY_POINT int main()
+#else
+#include<Windows.h>
+#define ENTRY_POINT int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nShowCmd)
+#endif // _DEBUG
 
-int main()
+#include <iostream>
+
+ENTRY_POINT 
 {
-    std::cout << "Hello World!\n";
+   std::cout << "Hello World!\n";
 }
