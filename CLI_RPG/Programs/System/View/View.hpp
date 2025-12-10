5e1181bae34929133ada5be0ab233cf432ed3b09
KDT-Takai <kdt24sgc40005@edu.kobe-tech.ac.jp>
2025-12-10 14:15:17 +0900

    View作ります

--- CLI_RPG/Programs/System/View/View.hpp
+++ CLI_RPG/Programs/System/View/View.hpp
@@ -1,13 +1,23 @@
 #pragma once
 #include <vector>
 #include <string>
+#include "../Singleton/Singleton.hpp"
+#include <iostream>
 
 // いったん仮で作る
-class View {
+class View : public Singleton<View> {
+	friend class Singleton<View>;
 public:
-    virtual ~View() = default;
 
-    virtual void Render(const std::vector<std::string>& messageLog) = 0;
+	// 描画
+	void RenderText(const std::string& text) {
+		// コンソールに出力
+		std::cout << text << std::endl;
+	}
 
-    virtual void Clear() = 0;
+	// エラー表示
+	void RenderError(const std::string& errorText) {
+		// コンソールにエラー出力
+		std::cerr << "\033[31m" << errorText << "\033[0m" << std::endl;
+	}
 };
