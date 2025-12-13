0dfd7b275384b7554291bbd6aaa4a3010a85fbfb
rento <koyo@303-22>
2025-12-13 15:05:36 +0900

    ファクトリー作った！ CharaTypeで種類を増やし、CharaFactory.cppでステータスを決めれるよ！

--- CLI_RPG/Programs/Chara/CharaFactory/CharaFactory.h
+++ CLI_RPG/Programs/Chara/CharaFactory/CharaFactory.h
@@ -1,1 +1,31 @@
 #pragma once
+#include <unordered_map>
+#include <memory>
+#include "../../Chara/Base/CharaBase.h"
+#include "../../Chara/CharaFactory/CharaType.h"
+#include "../../Chara/Enemy/Enemy.h" 
+#include "../../Chara/Player/Player.h"
+
+class CharaFactory
+{
+	std::unordered_map<CharaType, CharaData> charaDataMap;
+
+	// ステータス初期化用
+	void InitCharaData();
+
+public:
+
+	CharaFactory()
+	{
+		// ファクトリー生成時に各データを初期化
+		InitCharaData();
+	}
+	~CharaFactory() = default;
+
+	/// <summary>
+	/// 指定されたタイプのオブジェクトを生成
+	/// </summary>
+	/// <param name="type">生成したい敵のタイプ</param>
+	/// <returns>生成されたEnemyオブジェクトへのポインタ</returns>
+	std::unique_ptr<CharaBase> CreateChara(CharaType type) const;
+};
